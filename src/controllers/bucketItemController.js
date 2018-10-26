bf1d5171df6fe2b10c8299f11956df04e7f0033a
Nick Holden <nholden212@gmail.com>
2018-10-25 17:56:30 -0700

    Updating bucket items functional

--- src/controllers/bucketItemController.js
+++ src/controllers/bucketItemController.js
@@ -23,6 +23,28 @@ module.exports = {
         res.redirect("/");
       }
     })
+  },
+
+  renderEditForm(req, res, next){
+    bucketItemQueries.getBucketItem(req.params.id, (err, item) => {
+      if(err || item === null){
+        req.flash("There was a problem editing the item you selected.");
+        res.redirect(404, "/");
+      } else {
+        res.render("bucketItems/edit", {item});
+      }
+    })
+  },
+
+  updateBucketItem(req, res, next){
+    bucketItemQueries.updateBucketItem(req, req.body, (err, item) => {
+      if(err || item === null){
+        req.flash("notice", "There was a problem updating this item.");
+        res.redirect(404, `/bucketItems/${req.params.id}/edit`);
+      } else {
+        res.redirect("/users/profile");
+      }
+    })
   }
 
 }
